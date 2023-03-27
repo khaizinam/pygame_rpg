c66c87a8f1c5240f6ee3345ac581524b1068fdfd
Dinh The Vinh <dtvince0310@gmail.com>
2023-03-27 23:14:49 +0700

    add puzzle

--- Ground.py
+++ Ground.py
@@ -22,7 +22,11 @@ class Block(pygame.sprite.Sprite):
         self.collideAttack()
     
     def collideAttack(self):
-        hits = pygame.sprite.spritecollide(self, self.game.magic_attacks, True)
+        pygame.sprite.spritecollide(self, self.game.magic_attacks, True)
+        hits = pygame.sprite.spritecollide(self, self.game.puzzle, False)
+        if hits:
+            hits[0].reset()
+    
         
 class Wall(pygame.sprite.Sprite):
     def __init__(self, game , x, y):
@@ -46,6 +50,9 @@ class Wall(pygame.sprite.Sprite):
     
     def collideAttack(self):
         hits = pygame.sprite.spritecollide(self, self.game.magic_attacks, True)
+        hits = pygame.sprite.spritecollide(self, self.game.puzzle, False)
+        if hits:
+            hits[0].reset()
         
 class Grass(pygame.sprite.Sprite):
     def __init__(self, game, id, x, y):
@@ -82,4 +89,105 @@ class HeartItem(pygame.sprite.Sprite):
         self.rect.x = self.x
         self.rect.y = self.y
     def update(self ): pass
+
+class Cube(pygame.sprite.Sprite):
+    def __init__(self, game , x, y):
+        
+        self.game = game
+        self._layer = BLOCK_LAYER + 1
+        self.groups = self.game.all_sprites, self.game.puzzle, self.game.blocks
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        
+        self.posX = x*TILESIZE
+        self.posY = y*TILESIZE
+        self.x = self.posX
+        self.y = self.posY
+        print(self.x, self.y)
+        self.width =  34
+        self.height = 48
+        self.image = self.game.terrain_spritesheet.get_sprite( 96, 400, self.width , self.height)
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        self.inHole = False
+    def update(self):
+        self.collidePlayer()
     
+    def collidePlayer(self):
+        if not self.inHole:
+            player = self.game.player
+            if self.y <= player.y <= self.y + 16:
+                if player.x + player.rect.width + 10 >= self.x and player.x < self.x:
+                    self.x = player.x + player.rect.width + 10
+                elif player.x - 10 <= self.width + self.x and player.x > self.x:
+                    self.x = player.x - self.width - 10
+            if self.x <= player.x <= self.x + 10:
+                if player.y + player.rect.height + 10 >= self.y and player.y < self.y:
+                    self.y = player.y + player.rect.height + 10
+                elif player.y - 10 <= self.height + self.y and player.y > self.y:
+                    self.y = player.y - self.height - 10
+    
+    def reset(self):
+        self.x = self.posX
+        self.y = self.posY
+
+
+class Hole(pygame.sprite.Sprite):
+    def __init__(self, game , x, y):
+        
+        self.game = game
+        self._layer = BLOCK_LAYER
+        self.groups = self.game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        
+        self.x = x* TILESIZE
+        self.y = TILESIZE * y
+        self.width =  64
+        self.height = 66
+        self.image = self.game.terrain_spritesheet.get_sprite( 128, 384, self.width , self.height)
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+    def update(self):
+        self.collideAttack()
+    
+    def collideAttack(self):
+        hits = pygame.sprite.spritecollide(self, self.game.puzzle, False)
+        if hits:
+            cube = hits[0]
+            if self.x +16 <= cube.x <= self.x + 31 and self.y <= cube.y <= self.y + 25:
+                cube.inHole = True
+                cube.x = self.x + 16
+                cube.y = self.y
+
+class Gate(pygame.sprite.Sprite):
+    def __init__(self, game , x, y):
+        
+        self.game = game
+        self._layer = BLOCK_LAYER
+        self.groups = self.game.all_sprites, self.game.blocks
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        
+        self.x = x* TILESIZE
+        self.y = TILESIZE * y
+        self.width =  128
+        self.height = 34
+        self.image = self.game.terrain_spritesheet.get_sprite( 224, 640, self.width , self.height)
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def update(self):
+        if self.checkPuzzle():
+            self.kill()
+    
+    def checkPuzzle(self):
+        solved = True
+        for puzzle in self.game.puzzle:
+            solved = solved and puzzle.inHole
+        return solved
+                
--- Player.py
+++ Player.py
@@ -27,7 +27,7 @@ class Player(pygame.sprite.Sprite):
         self.curentExp = 0
         self.nextExp = self.level * 100
         self.magicRange = 5 + 0.5 * self.level
-        self.magicReduce = 25 - 2 * self.level
+        self.magicReduce = 40 - 2 * self.level
         if self.magicReduce <= 6: self.magicReduce = 6
         self.magicTime = 0
         #-----------
@@ -136,11 +136,11 @@ class Player(pygame.sprite.Sprite):
         MagicAttack(self)
         
     def attacked(self, damge):
-        #pass
-        self.hp -= damge
-        if self.hp <= 0:
-            self.kill()
-            self.game.playing = False
+        pass
+        # self.hp -= damge
+        # if self.hp <= 0:
+        #     self.kill()
+        #     self.game.playing = False
             
         
     def collide_blocks(self, direction):
